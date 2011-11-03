e4d0173f60fa0110585846d9ba92b105a593c7ad
AD11 Koddojo <dojo@kgm.cc>
2011-11-03 11:20:35 +0100

    lade upp uppgifterna som kommentarer i index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+// lab3 om klasser
+
+// 1. skapa en kundklass
+// 2. skapa tre kunder med/egenskaper
+// 6. skapa 5 kunder via konstruktorn
+// 7. skapa getters & setters för egenskaper
+// 8.1 skapa objekt & lägg i en array.
+// 8.2 loopa igenom array, skriv ut svar på frågor
+// 9. skriv om frågorna till metoder i objektet
+// 10. skriv regexp för att validera kundegenskaper 
+// 10? innan de sätts i formulär eller i konstruktorn?
+
+// 1. skapa en kundklass
+
+class
+
+/*
+namn
+adress
+ålder
+epost
+tele
+hårfärg
+*/
+
+?>
