6496388d7d467ca14ff889cb3be8955674230d7d
AD11 Koddojo <dojo@kgm.cc>
2011-11-03 11:38:18 +0100

    skapat setter för name i Customer

--- index.php
+++ index.php
@@ -4,21 +4,10 @@
 
 // 1. skapa en kundklass
 
-// 2. skapa tre kunder med/egenskaper
-// 6. skapa 5 kunder via konstruktorn
-// 7. skapa getters & setters för egenskaper
-// 8.1 skapa objekt & lägg i en array.
-// 8.2 loopa igenom array, skriv ut svar på frågor
-// 9. skriv om frågorna till metoder i objektet
-// 10. skriv regexp för att validera kundegenskaper 
-// 10? innan de sätts i formulär eller i konstruktorn?
-
-// 1. skapa en kundklass
-
 class Customer
 {
 	// attribut
-	public $name; // en sträng
+	private $name; // en sträng
 	public $address; // en sträng
 	public $age;	// en int
 	public $email; // en sträng
@@ -26,9 +15,38 @@ class Customer
 	public $hair; // en sträng
 
 	// metoder
+	function set_name($in_name)
+	{
+		$this->name = $in_name;
+	}
 
 }
 
+// 2. skapa tre kunder med/egenskaper
+
+$cust1 = new Customer();
+
+//$cust1->name = "Kristian";
+$cust1->set_name("Kristian");
+
+echo "<pre>";
+print_r( $cust1 );
+echo "</pre>";
+
+
+$cust2 = new Customer();
+$cust3 = new Customer();
+
+
+
+
+// 6. skapa 5 kunder via konstruktorn
+// 7. skapa getters & setters för egenskaper
+// 8.1 skapa objekt & lägg i en array.
+// 8.2 loopa igenom array, skriv ut svar på frågor
+// 9. skriv om frågorna till metoder i objektet
+// 10. skriv regexp för att validera kundegenskaper 
+// 10? innan de sätts i formulär eller i konstruktorn?
 
 
 ?>
